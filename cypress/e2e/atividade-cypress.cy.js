d22b35022a25bd81572dca167a81c2944c5caf17
luiszinhob <luisfelipeoliveiralima615@gmail.com>
2023-08-15 20:01:50 -0300

    cypress-login

---
+++ cypress/e2e/atividade-cypress.cy.js
@@ -0,0 +1,35 @@
+describe('Teste Cypress', () => {
+  it('Testar Cadastro', () => {
+    cy.visit('http://teste-cypress.s3-website-us-east-1.amazonaws.com/')
+  
+  
+cy.get('[placeholder="Email"]').type('cypress@teste.com')
+cy.get('[placeholder="senha"]').type('cypress')
+cy.contains('Ir para o Cadastro').click()
+
+cy.get('[placeholder="Nome Completo"]').type('teste')
+cy.get('[placeholder="Email"]').type('cypress@teste.com')
+cy.get('[placeholder="senha"]').type('cypress')
+cy.get('[placeholder="Confirme a senha"]').type('cypress')
+cy.contains('Criar conta').click()
+cy.contains('Você está logado!').should('be.visible')
+ })
+it('Testar Login', () => {
+
+    cy.visit('http://teste-cypress.s3-website-us-east-1.amazonaws.com/')
+    cy.get('[placeholder="Email"]').type('cypress@teste.com')
+    cy.get('[placeholder="senha"]').type('cypress')
+    cy.get('button[type="submit"').click()
+    cy.contains('Você está logado!').should('be.visible')
+  })
+})
+
+
+
+
+
+
+
+
+
+
